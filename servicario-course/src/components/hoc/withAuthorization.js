f5afc5ab5965a278e9d44764e4b11a8562c954ce
abhishekptiwari <abhishektiwariaman@gmail.com>
2022-02-26 21:01:41 +0530

    git localization

---
+++ servicario-course/src/components/hoc/withAuthorization.js
@@ -0,0 +1,26 @@
+
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+
+
+const withAuthorization = Component => {
+  class WithAuthorization extends React.Component {
+
+    render() {
+      const { auth } = this.props
+      return auth.isAuth ? <Component {...this.props} /> : <Redirect to="/login" />
+    }
+  }
+
+  return connect(({auth}) => ({auth}))(WithAuthorization)
+}
+
+
+export default withAuthorization
+
+
+
+
+
---
+++ servicario-course/src/pages/Secret.js
@@ -0,0 +1,14 @@
+
+import withAuthorization from 'components/hoc/withAuthorization'
+
+
+
+import React from 'react'
+
+const Secret = (props) => {
+  return (
+    <h1>I am SECRET Page, ONLY Auth USER can see me!</h1>
+  )
+}
+
+export default withAuthorization(Secret)
